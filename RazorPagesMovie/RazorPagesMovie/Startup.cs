08954d4bf9d3e00a3ad821f63146da0fca106bf9
André Davis <cubicle.jockey@live.com>
2018-06-05 18:41:44 -0700

    Cleaned up and ready for Razor Pages. Reviewed what each page does and new tags for environments on Razor Pages.

--- RazorPagesMovie/RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/RazorPagesMovie/Startup.cs
@@ -26,8 +26,8 @@ namespace RazorPagesMovie
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
