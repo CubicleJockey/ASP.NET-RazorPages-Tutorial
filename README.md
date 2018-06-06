bf430df873530599c587387b311dddd5976b00b3
André Davis <cubicle.jockey@live.com>
2018-06-05 17:59:24 -0700

    Okay, ready to start after update.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ASP.NET-RazorPages-Tutorial
+
+##### Pre-Reqs:
+* [Visual Studio Community Download](https://www.visualstudio.com/vs/community/)
+* [Dot Net Download](https://download.microsoft.com/download/8/8/5/88544F33-836A-49A5-8B67-451C24709A8F/dotnet-sdk-2.1.300-win-gs-x64.exe)
+<br>
+##### Tutorials and Documentation:
+* [Razor Pages Tutorial](https://docs.microsoft.com/en-us/aspnet/core/tutorials/razor-pages/razor-pages-start?view=aspnetcore-2.1)
+* [.Net CLI Documentation](https://docs.microsoft.com/en-us/dotnet/core/tools/?tabs=netcore2x)
+* [Markdown Guide](https://guides.github.com/features/mastering-markdown/)
--- RazorPagesMovie/RazorPagesMovie/Program.cs
+++ RazorPagesMovie/RazorPagesMovie/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace RazorPagesMovie
 {
@@ -17,7 +10,7 @@ namespace RazorPagesMovie
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
     }
