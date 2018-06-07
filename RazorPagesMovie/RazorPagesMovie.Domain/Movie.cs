dfe9328e7e73ab54d74a682bb712435047973081
André Davis <CubicleJockey@users.noreply.github.com>
2018-06-07 14:05:39 -0700

    Data Annotations and self-documenting code changes

--- RazorPagesMovie/RazorPagesMovie.Domain/Movie.cs
+++ RazorPagesMovie/RazorPagesMovie.Domain/Movie.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Domain
 {
@@ -6,8 +9,14 @@ namespace RazorPagesMovie.Domain
     {
         public int Id { get; set; }
         public string Title { get; set; }
+
+        [DisplayName("Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        //data annotation is required so Entity Framework Core can correctly map Price to currency in the database.
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
