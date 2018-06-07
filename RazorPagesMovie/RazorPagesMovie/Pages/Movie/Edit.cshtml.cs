2c5247329d3407e2df68b8d998e3c457178c65ab
André Davis <cubicle.jockey@live.com>
2018-06-07 11:13:25 -0700

    moving to desktop

--- RazorPagesMovie/RazorPagesMovie/Pages/Movie/Edit.cshtml.cs
+++ RazorPagesMovie/RazorPagesMovie/Pages/Movie/Edit.cshtml.cs
@@ -1,9 +1,8 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.EF;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RazorPagesMovie.Pages.Movie
 {
